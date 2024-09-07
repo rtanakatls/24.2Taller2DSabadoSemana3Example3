bb1112ef6646b2a35a13552fa5fba0de291ed2e2
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2024-09-07 11:15:32 -0500

    Created items

--- Store.cs
+++ Store.cs
@@ -8,9 +8,21 @@ namespace _24._2Taller2DSabadoTienda
 {
     internal class Store
     {
+        private List<Item> items;
+        private List<Item> cart;
+        private float money;
+
         public void Execute()
         {
+            CreateItems();
+        }
 
+        private void CreateItems()
+        {
+            items = new List<Item>();
+            items.Add(new Item("Leche", 10, 100));
+            items.Add(new Item("Huevos", 5, 20));
+            items.Add(new Item("Queso", 15, 50));
         }
     }
 }
